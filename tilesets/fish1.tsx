0df3dc88f799b9ce153d80c0e3a92b568454f195
eyalron33 <eyalron33@gmail.com>
2022-11-12 00:09:27 +0100

    add citadef deocrations

---
+++ tilesets/fish1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="fish1" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="../../../../../Downloads/fish1.png" width="110" height="95"/>
+</tileset>
