669d10d07e7f0b429125c094eb9faddea49ea9e2
eyalron33 <eyalron33@gmail.com>
2022-11-12 07:54:08 +0100

    remove new additions to see if github action works

--- tilesets/aquarium.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="aquarium" tilewidth="32" tileheight="32" tilecount="42" columns="7">
- <image source="../../../../../Downloads/aquarium.png" width="250" height="205"/>
-</tileset>
--- tilesets/citafish1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="citafish1" tilewidth="32" tileheight="32" tilecount="30" columns="6">
- <image source="../../../../../Downloads/citafish1.png" width="200" height="172"/>
-</tileset>
